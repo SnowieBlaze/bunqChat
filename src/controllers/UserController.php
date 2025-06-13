6f6e3f7e5149fea4b81350f917b35b52c1cf2e5f
Alexandru Theodor Tarcuta <a.t.tarcuta@student.tudelft.nl>
2025-06-13 05:24:47 +0200

    Created user controller

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,7 +2,63 @@
 
 namespace controllers;
 
+use \models\UserModel;
+use PDO;
+
 class UserController
 {
+    private UserModel $userModel;
 
+    /**
+     * Constructor that instantiates userModel with a database object.
+     * @param PDO $db - the database object.
+     */
+    public function __construct(PDO $db)
+    {
+        $this->userModel = new UserModel($db);
+    }
+
+    /**
+     * Create a new user.
+     * @param string $username - The username from request.
+     * @return array - Success status and user Id if successful.
+     *                 False and error otherwise.
+     */
+    public function createUser(string $username): array
+    {
+        try {
+            $user_id = $this->userModel->createUser($username);
+            return [
+                "success" => true,
+                "user_id" => $user_id,
+            ];
+        } catch (\Exception $e) {
+            return [
+                "success" => false,
+                "error" => $e->getMessage(),
+            ];
+        }
+    }
+
+    /**
+     * Get user data by ID.
+     * @param int $id
+     * @return array|null - User data array if found.
+     *                      Null otherwise.
+     */
+    public function getUserById(int $id): ?array
+    {
+        return $this->userModel->getUserById($id);
+    }
+
+    /**
+     * Get user data by username.
+     * @param string $username
+     * @return array|null - User data array if found.
+     *                      Null otherwise.
+     */
+    public function getUserByUsername(string $username): ?array
+    {
+        return $this->userModel->getUserByUsername($username);
+    }
 }
