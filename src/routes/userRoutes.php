5754076644b85c782fa559d6e435359fea61fb31
Alexandru Theodor Tarcuta <a.t.tarcuta@student.tudelft.nl>
2025-06-13 06:20:39 +0200

    Added routes for messages as well

--- src/routes/userRoutes.php
+++ src/routes/userRoutes.php
@@ -9,28 +9,28 @@ return function ($app, PDO $db) {
     /**
      * The route for creating a new user.
      */
-    $app->post('/user', function ($request, $response) use ($userController) {
+    $app->post("/user", function ($request, $response) use ($userController) {
         $data = $request->getParsedBody();
-        $result = $userController->createUser($data['username'] ?? '');
+        $result = $userController->createUser($data["username"] ?? "");
         $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
+        return $response->withHeader("Content-Type", "application/json");
     });
 
     /**
      * The route for getting a user by id.
      */
-    $app->get('/user/{id}', function ($request, $response, $args) use ($userController) {
-        $result = $userController->getUserById((int)$args['id']);
+    $app->get("/user/{id}", function ($request, $response, $args) use ($userController) {
+        $result = $userController->getUserById((int)$args["id"]);
         $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
+        return $response->withHeader("Content-Type", "application/json");
     });
 
     /**
      * The route for getting a user by username.
      */
-    $app->get('/user/username/{username}', function ($request, $response, $args) use ($userController) {
-        $result = $userController->getUserByUsername($args['username']);
+    $app->get("/user/username/{username}", function ($request, $response, $args) use ($userController) {
+        $result = $userController->getUserByUsername($args["username"]);
         $response->getBody()->write(json_encode($result));
-        return $response->withHeader('Content-Type', 'application/json');
+        return $response->withHeader("Content-Type", "application/json");
     });
 };
